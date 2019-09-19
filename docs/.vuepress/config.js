bc5983ed0eed958cbede958f7ee21ad7586f94bc
hongtanhao <antartec@outlook.com>
2019-09-18 19:34:40 -0800

    vue components lib

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  base: '/',
+  title: 'AmandaVue',
+  ga: 'UA-85991013-5',
+  description: '打造一套属于自己的装备',
+  head: [
+      ['link', { rel: 'icon', href: '/logo.png' }],
+      ['script', { async: 'async', type: 'text/javascript' }, 'var _hmt=_hmt||[];!function(){var e=document.createElement("script");e.src="https://hm.baidu.com/hm.js?22a0ab0b25c4b63f25e294e1585f3aa5";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();']
+  ],
+  markdown: {
+      toc: {
+          includeLevel: [2, 3, 4, 5, 6]
+      }
+  },
+  themeConfig: {
+      repo: 'amanda-vue',
+      docsDir: 'docs',
+      editLinks: true,
+      editLinkText: '错别字纠正',
+      sidebarDepth: 3,
+      nav: [{
+              text: '正文',
+              link: '/art/',
+          },
+          {
+              text: 'MyBlog',
+              link: '/donate/'
+          },
+          {
+              text: '关于',
+              link: '/about/'
+          }
+      ],
+      sidebar: {
+          '/art/': [
+              ''
+          ]
+      }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 开始阅读
+actionLink: /art/
+features:
+    - title: 多学习
+      details: 尘世浮躁，保持一额淡静好学的心，实在宝贵。
+    - title: 多总结
+      details: 重蹈覆辙，这真的不能原谅，所以平时归纳总结尤为重要。
+    - title: 多动手
+      details: 看得明白不一定写的清楚。
+footer: Copyright © 2019/09/18  --峰
+---
